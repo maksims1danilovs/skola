c363faa0e2b6baccbcdde5a369f9d102a681cad6
maksims1danilovs <maksims.danilovs@seznam.cz>
2025-09-04 18:24:12 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,36 @@
-obdobi = "léto"
+rychlost = int(input("Vase rychlost: "))
 
-if obdobi == "zima":
-  print("Máma ti vzkazuje:", "Vezmi si kulicha!")
+print("1. dalnice \n2. mimo obec \n3. obec")
+lokace = int(input("Zadejte cislo lokace: "))
 
-if obdobi == "léto":
-  print("Negr Bagr Jaromir Jagr")
+# >>> TODO: zde napište svůj kód
+rychlost_obec = 50
+rychlost_mimo_obec = 90
+rychlost_dalnice = 130
+
+if lokace == 1:
+    print("Pozice auta:dalnice")
+    if rychlost <= rychlost_dalnice:
+        print("OK")
+    else:
+        print("Vysoka rychlost")
+    
+
+
+
+elif lokace== 2:
+    print("Pozice auta:mimo obec")
+    if rychlost <= rychlost_mimo_obec:
+        print("OK")
+    else:
+        print("Vysoka rychlost")
+
+
+
+elif lokace== 3:
+    print("Pozice auta:obec")
+    if rychlost <= rychlost_obec:
+        print("OK")
+    else:
+        print("Vysoka rychlost")
+
